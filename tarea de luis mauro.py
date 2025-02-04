19fb54dde70717191924fc09b0cd9ab6ff4df8e1
Luismauromedina <luismauromedina111@gmail.com>
2025-02-04 14:36:48 -0400

    Add files via upload

---
+++ tarea de luis mauro.py
@@ -0,0 +1,10 @@
+nombre = input ("por favor , ingresa tu nombre: ")
+edad = int(input(" por favor, ingresa tu edad: "))
+pais = input("¿ de que pais eres? ")
+
+print(f"hola, {nombre}. tienes {edad} años y eres de {pais}.")
+
+luis = int(input(" ingresa numero 1: "))
+mauro = int (input(" ingresa numero 2: "))
+
+print(f"hola luis, {luis}. y tengo: , y voy a cumpli {mauro}. ")
